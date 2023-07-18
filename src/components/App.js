29d03027cc09b0f207b7666104168626bd9ad35c
ameynagulwar <130839577+ameynagulwar@users.noreply.github.com>
2023-07-18 16:12:21 +0530

    submitting code-Tue Jul 18 2023 16:12:17 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,16 @@ import './../styles/App.css';
 import Child from "./Child.js"
 const App = () => {
   let [modal,setModal] = useState(false)
-
   return (
     <div className="parent">
        <h1>Parent Component</h1>
        <Child changeModal = {setModal}/>
-       <div id="mod" style={{display:"none"}}>
-        <h3>Modal Content</h3>
-        <p>This is modal content</p>
-       </div>
+       {
+         modal && <div>
+          <h3>Modal Content</h3>
+          <p>This is the modal Content.</p>
+         </div>
+       }
     </div>
   )
 }
